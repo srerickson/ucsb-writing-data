d72a06b2964d174f6865261b2a9ce005bcc2ec10
Seth Erickson <sr.erickson@gmail.com>
2024-06-25 12:28:33 -0700

    import and embedding workflow

--- embeddings.py
+++ embeddings.py
@@ -2,56 +2,70 @@ import pathlib
 import ollama
 import duckdb
 from duckdb.typing import VARCHAR
-import pandas as pd
-from typing import Sequence
+from typing import Sequence         
 
-model = "mxbai-embed-large"
-db_path = str(pathlib.Path(f'outputs/embeddings-{model}.db'))
+src_path = pathlib.Path('data/reflections.csv')
+db_path = pathlib.Path(f'outputs/reflections.duckdb')
 
 create_table_sql = """
-    CREATE TABLE IF NOT EXISTS embeddings(
+    CREATE SEQUENCE IF NOT EXISTS seq_text_responses_id START 1;
+    CREATE TABLE IF NOT EXISTS text_responses (
+        id INTEGER PRIMARY KEY,
         student_id VARCHAR,
-        response_id VARCHAR,
+        question_id VARCHAR,
         response_text VARCHAR,
-        embedding FLOAT[1024],
-        UNIQUE(student_id, response_id)
-    );         
+        UNIQUE(student_id, question_id)
+    );
 """
 
-insert_response_sql = """
-    INSERT INTO embeddings (
+import_responses_sql = f"""
+    INSERT INTO text_responses (
+        id,
         student_id,
-        response_id,
+        question_id,
         response_text
-    ) VALUES (?, ?, ?) ON CONFLICT DO NOTHING;
+    ) SELECT
+        nextval('seq_text_responses_id'),
+        perm as student_id, 
+        question_id, 
+        response_text
+    FROM (
+        UNPIVOT '{str(src_path)}' ON r1, r2, r3, r4 INTO NAME 'question_id' VALUE 'response_text'
+    ) ON CONFLICT DO NOTHING;  
 """
 
-def embedding(text :str) -> Sequence[float] | None:
+mxbai_embed_large_sql = """
+    CREATE TABLE IF NOT EXISTS mxbai_embed_large (
+        id INTEGER PRIMARY KEY,
+        embedding FLOAT[1024],
+        FOREIGN KEY (id) REFERENCES text_responses (id)
+    );
+    INSERT INTO mxbai_embed_large (id, embedding)
+    SELECT 
+        resp_id,
+        do_mxbai_embed_large(resp_text)
+    FROM (
+        SELECT 
+            text_responses.id as resp_id,
+            text_responses.response_text as resp_text,
+            mxbai_embed_large.embedding as embedding
+        FROM text_responses
+        LEFT OUTER JOIN mxbai_embed_large 
+            ON mxbai_embed_large.id = text_responses.id
+        WHERE embedding is NULL
+    );
+"""
+
+def mxbai_embed_large(text :str) -> Sequence[float] | None:
+    model = "mxbai-embed-large"
     try:
         response = ollama.embeddings(model=model, prompt=text)
         return response["embedding"]
     except ollama._types.ResponseError as e:
         return None
 
-with duckdb.connect(database=db_path) as conn:
+with duckdb.connect(database=str(db_path)) as conn:
+    conn.create_function('do_mxbai_embed_large', mxbai_embed_large, [VARCHAR], 'FLOAT[1024]')
     conn.execute(create_table_sql)
-    # conn.create_function("get_embedding", embedding, [VARCHAR], 'FLOAT[1024]')
-    df = pd.read_csv(pathlib.Path("data/reflections.csv"))
-    
-    def upsert_responses(row: pd.core.series.Series):
-        for col in ['r1', 'r2', 'r3', 'r4']:
-            student_id = row['perm']
-            response_text = row[col]
-            conn.execute(insert_response_sql, [student_id, col, response_text])
-    
-    # for col in ['r1', 'r2', 'r3', 'r4']:
-    df.apply(upsert_responses, axis=1)
-
-
-
-
-
-# duckdb.connect()
-
-
-# df.to_csv("outputs/reflections_embeddings.csv")
+    conn.execute(import_responses_sql)
+    conn.execute(mxbai_embed_large_sql)
